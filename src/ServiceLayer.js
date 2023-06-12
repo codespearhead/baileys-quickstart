35fce44a000d475106bfb18e571896793cc1274f
Paguiar735 <72931357+Paguiar735@users.noreply.github.com>
2023-06-12 01:38:32 -0400

    [REFACTOR]: add now refactored business logic

--- src/ServiceLayer.js
+++ src/ServiceLayer.js
@@ -49,7 +49,22 @@ module.exports = class ServiceLayer {
   }
 
   // Uncomment the lines below to implement your business logic
-  // static async realBusinessLogic(sock, msg_txt, msg_destinatary) {
-  // }
-
+  static async realBusinessLogic(sock, msg_txt, msg_destinatary) {
+    msg_txt = msg_txt.replace(/\D+/g, "");
+    let msg_reply;
+    switch (true) {
+      case msg_txt.length === 8:
+        msg_reply = `Oi, eu sou o Jarvis! Aqui está o seu link:\n\nhttps://wa.me/5592${msg_txt}`;
+        break;
+      case msg_txt.length > 8:
+        msg_reply = `Parece que sua mensagem possui mais que oito dígitos.\n\nSegue o link considerando apenas os oito últimos dígitos de sua mensagem:\n\nhttps://wa.me/5592${msg_txt.slice(
+          -8
+        )}`;
+        break;
+      default:
+        msg_reply = `Parece que sua mensagem possui menos que oito dígitos.\n\nFavor digitar os oito últimos dígitos do número de telefone desejado.`;
+        break;
+    }
+    await sock.sendMessage(msg_destinatary, { text: msg_reply });
+  }
 };
